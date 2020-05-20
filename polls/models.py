83746db55e995f28b4ade173c29e1e3f66f42aaf
soumya9461 <soumya.awasthi08@gmail.com>
2020-05-20 18:32:24 +0530

    Views.py is made generic

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 class choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
